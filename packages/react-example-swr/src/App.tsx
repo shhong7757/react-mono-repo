2c512bee8a22abc19f04fa7223bee1c4133bc9cf
shhong7757 <ghdtkdgur123@gmail.com>
2021-07-28 14:20:00 +0900

    feat: suspense 예제 적용

--- packages/react-example-swr/src/App.tsx
+++ packages/react-example-swr/src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Button } from '@react-mono-repo/react-common-component';
 
 import { hot } from 'react-hot-loader/root';
 import { SWRConfig } from 'swr';
 
+import ErrorBoundary from './ErrorBoundary';
+
 import useRandomDog from './hooks/useRandomDog';
 
 function App() {
-  const { data, isError, isLoading } = useRandomDog();
-
-  if (isLoading) return <div>loading...</div>;
-  if (isError) return <div>failed to load</div>;
+  const { data } = useRandomDog();
 
   return (
-    <SWRConfig value={{}}>
-      <div>
-        <img alt="" src={data} />
-      </div>
+    <SWRConfig value={{ suspense: true }}>
+      <ErrorBoundary fallback={<div>failed to load</div>}>
+        <Suspense fallback={<div>loading...</div>}>
+          <div>
+            <img alt="" src={data} />
+          </div>
+        </Suspense>
+      </ErrorBoundary>
       <Button>
         <p>Hello! world</p>
       </Button>
---
+++ packages/react-example-swr/src/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface Props {
+  fallback?: React.ReactElement;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return {
+      hasError: true,
+    };
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { fallback, children } = this.props;
+
+    if (hasError) {
+      return fallback;
+    }
+    return children;
+  }
+}
+
+export default ErrorBoundary;
